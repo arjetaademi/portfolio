e5e2a6fc33544b4ef0db6f2b297132395d68736b
Arjeta Ademi <arjetaademi47@gmail.com>
2026-06-05 14:20:19 +0200

    Fix App import

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
-import CV  from "./components/CV";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -25,7 +24,6 @@ export default function App() {
       <Projects />
       <Education />
       <Experience />
-      <CV />
       <Contact />
       <Footer />
     </>
